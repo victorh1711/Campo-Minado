fbdd24b0f087f060f3dc97f5a6f1a85b670f2607
victorh1711 <victorhugosilva170711@gmail.com>
2026-05-06 17:38:13 -0300

    melhoramos a responsividade do projeto em dispositivos moveis para facilitar o uso e jogabilidade

--- JS/field.js
+++ JS/field.js
@@ -1,114 +1,122 @@
-import * as Global from './game.js'
+    import * as Global from './game.js'
 
-export let maxBombs = null
-export let gridSize = null
-let fontSize = null
-export const isBombField = new Set();
+    export let maxBombs = null
+    export let cols = null
+    export let rows = null
+    export const isBombField = new Set();
 
-export function setGrid(){
-    const grid = Global.game.selectGrid.value
+    let fontSize = null
 
-    if(grid == "easy"){
-        fontSize = "200%"
-        gridSize = 7
-        maxBombs = 7
-    } 
-    else if(grid == "normal"){
-        fontSize = "150%"
-        gridSize = 11
-        maxBombs = 20
-    }
-    else if(grid == "hard"){
-        fontSize = "110%"
-        gridSize = 15
-        maxBombs = 35
-    }
-    Global.game.board.style.setProperty("--font-size", fontSize);
-    Global.game.board.style.setProperty("--grid-size", gridSize);
-}
+    export function setGrid(){
+        const grid = Global.game.selectGrid.value
+        const isMobile = window.innerWidth < 600 || 'ontouchstart' in window
 
-export function createTable() {
-    Global.game.board.innerHTML = "";
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        const field = document.createElement("div");
-        field.classList.add("field");
-        field.dataset.index = i;
-        Global.game.board.appendChild(field);
-    }
-    Global.game.fields = document.querySelectorAll(".field");
-    Global.game.flagsCounter.textContent = maxBombs;
-
-}
-
-export function createBombs(firstIndex) {
-    while (isBombField.size < maxBombs) {
-        const randomIndex = Math.floor(Math.random() * Global.game.fields.length);
-
-        if (randomIndex === firstIndex || isBombField.has(randomIndex)) continue;
-
-        isBombField.add(randomIndex);
-    }
-}
-
-export function revealAllBombs() {
-    isBombField.forEach((bombIndex) => {
-        const field = Global.game.fields[bombIndex];
-
-        field.classList.add("field--is-bomb");
-
-        if (field.classList.contains("field--flagged")) {
-            field.classList.remove("field--flagged");
-            Global.game.flagsCounter.textContent++;
+        if (grid === "easy") {
+            rows = 7;
+            cols = 7;
+            maxBombs = 7;
+            fontSize = '200%'
         }
 
-        const bombIcon = document.createElement("div");
-        bombIcon.classList.add("bomb-icon");
-        field.appendChild(bombIcon);
-    });
-}
+        if (grid === "normal") {
+            rows = 11;
+            cols = isMobile ? 8 : 11;
+            maxBombs = isMobile ? 14 : 20;
+            fontSize = '150%'
+        }
 
-export function getNeighbors(index) {
-    const row = Math.floor(index / gridSize);
-    const col = index % gridSize;
-    const neighbors = [];
+        if (grid === "hard") {
+            rows = 15;
+            cols = isMobile ? 10 : 15;
+            maxBombs = isMobile ? 27 : 40;
+            fontSize = '110%'
+        }
+        Global.game.board.style.setProperty("--font-size", fontSize);
+        Global.game.board.style.setProperty("--grid-size", cols);
+    }
 
-    for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-            if (i === 0 && j === 0) continue;
+    export function createTable() {
+        Global.game.board.innerHTML = "";
+        for (let i = 0; i < rows * cols; i++) {
+            const field = document.createElement("div");
+            field.classList.add("field");
+            field.dataset.index = i;
+            Global.game.board.appendChild(field);
+        }
+        Global.game.fields = document.querySelectorAll(".field");
+        Global.game.flagsCounter.textContent = maxBombs;
 
-            const newRow = row + i;
-            const newCol = col + j;
+    }
 
-            if (newRow >= 0 && newRow < gridSize && newCol >= 0 && newCol < gridSize) {
-                neighbors.push(newRow * gridSize + newCol);
+    export function createBombs(firstIndex) {
+        while (isBombField.size < maxBombs) {
+            const randomIndex = Math.floor(Math.random() * Global.game.fields.length);
+
+            if (randomIndex === firstIndex || isBombField.has(randomIndex)) continue;
+
+            isBombField.add(randomIndex);
+        }
+    }
+
+    export function revealAllBombs() {
+        isBombField.forEach((bombIndex) => {
+            const field = Global.game.fields[bombIndex];
+
+            field.classList.add("field--is-bomb");
+
+            if (field.classList.contains("field--flagged")) {
+                field.classList.remove("field--flagged");
+                Global.game.flagsCounter.textContent++;
+            }
+
+            const bombIcon = document.createElement("div");
+            bombIcon.classList.add("bomb-icon");
+            field.appendChild(bombIcon);
+        });
+    }
+
+    export function getNeighbors(index) {
+        const row = Math.floor(index / cols);
+        const col = index % cols;
+        const neighbors = [];
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) continue;
+
+                const newRow = row + i;
+                const newCol = col + j;
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+                    neighbors.push(newRow * cols + newCol);
+                }
             }
         }
+
+        return neighbors;
     }
 
-    return neighbors;
-}
-
-export function countBombs(index) {
-    return getNeighbors(index).filter(n => isBombField.has(n)).length;
-}
-
-export function revealField(index) {
-    const field = Global.game.fields[index];
-
-    if (
-        field.classList.contains("field--revealed") ||
-        field.classList.contains("field--flagged")
-    ) return;
-
-    field.classList.add("field--revealed");
-
-    const bombsAround = countBombs(index);
-
-    if (bombsAround > 0) {
-        field.textContent = bombsAround;
-        field.dataset.number = bombsAround;
-        return;
+    export function countBombs(index) {
+        return getNeighbors(index).filter(n => isBombField.has(n)).length;
     }
-    
-    getNeighbors(index).forEach(n => revealField(n));
-}
+
+    export function revealField(index) {
+        const field = Global.game.fields[index];
+
+        if (
+            field.classList.contains("field--revealed") ||
+            field.classList.contains("field--flagged")
+        ) return;
+
+        field.classList.add("field--revealed");
+
+        const bombsAround = countBombs(index);
+
+        if (bombsAround > 0) {
+            field.textContent = bombsAround;
+            field.dataset.number = bombsAround;
+            return;
+        }
+        
+        getNeighbors(index).forEach(n => revealField(n));
+    }
